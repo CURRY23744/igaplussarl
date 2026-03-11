3c1f9e6bcb70fc507ad81080f45ac7557fc8b5d2
Isabelle AKAKPO <isabelleakakpo21@gmail.com>
2026-03-11 13:54:26 +0000

    fix:changement

--- docker-start.sh
+++ docker-start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# Remplace ${PORT} dans la config Nginx par la vraie valeur fournie par Railway
-sed -i "s/\${PORT}/${PORT}/g" /etc/nginx/sites-available/default
+# 1. On injecte le port de Railway dans la config Nginx
+# Railway donne un port (ex: 1234), on doit dire à Nginx d'écouter dessus
+sed -i "s/listen 80;/listen ${PORT};/g" /etc/nginx/sites-available/default
 
-# Lancer PHP-FPM en arrière-plan
+# 2. On lance PHP-FPM en arrière-plan (&)
 php-fpm -D
 
-# Lancer Nginx au premier plan
+# 3. On lance Nginx au PREMIER PLAN (indispensable pour Docker)
 nginx -g "daemon off;"
